e5758f74d406b2b2c110061c703220cdbdd9e794
The _IsaacAgboola <isaacadeyemi9@gmail.com>
2024-05-23 11:12:13 +0100

    components

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import API from "./services/API.js";
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
 
+//linking the created web component
+import { MenuPage } from "./components/MenuPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+
 //to make these imported modules global; we can simply attach them to the global window object METHOD 1
 window.app = {};
 app.store = Store;
